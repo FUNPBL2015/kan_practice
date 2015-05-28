35a38c9d0b77ba6cf9b9d8b18cb9dbf41fc93269
Kenta Iwami <b1013068@fun.ac.jp>
2015-05-28 23:50:17 +0900

    first

---
+++ practice1/practice1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice1
+//
+//  Created by 岩見建汰 on 2015/05/27.
+//  Copyright (c) 2015年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
